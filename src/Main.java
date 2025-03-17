357af8d9f07f7adcf8f0de4a2965e5f88dc01a10
Dorian Di Lillo <d.dilillo@stag.technofuturtic.education>
2025-03-17 14:23:29 +0100

    the good code pls

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    static void printArray(int[] array) {
+        System.out.print("Array: {");
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
         }
+        System.out.println(array[array.length - 1] + "}");
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        printArray(arr);
+
+        System.out.println("entrez 3 mots");
+        String mot1 = sc.next();
+        String mot2 = sc.next();
+        String mot3 = sc.next();
+        System.out.println(mot1);
+        System.out.println(mot2);
+        System.out.println(mot3);
     }
 }
