98bbb13c7deefe607f8a3d20901ff6d795518f73
Dorian Di Lillo <d.dilillo@stag.technofuturtic.education>
2025-03-20 14:54:45 +0100

    array 1d and 2d

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,27 @@ public class Main {
         }
         System.out.println(array[array.length - 1] + "}");
     }
+    static void printArray(int[][] array) {
+        System.out.print("Array: {");
+        for (int i = 0; i < array.length-1; i++) {
+            System.out.print("{");
+            for (int j = 0; j < array[i].length - 1; j++) {
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.print(array[i][array[array.length-1].length - 1] + "}, ");
+        }
+        System.out.print("{");
+        for (int i = 0; i < array[array.length - 1].length - 1; i++) {
+            System.out.print(array[array.length - 1][i] + ", ");
+        }
+        System.out.print(array[array.length - 1][array[array.length-1].length - 1] + "}");
+        System.out.println("}");
+    }
 
     public static void main(String[] args) {
-        System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        printArray(arr);
-
-        System.out.println("entrez 3 mots");
-        String mot1 = sc.next();
-        String mot2 = sc.next();
-        String mot3 = sc.next();
-        System.out.println(mot1);
-        System.out.println(mot2);
-        System.out.println(mot3);
+//        System.out.println("Enter the number of elements you wish to enter: ");
+//        int n = sc.nextInt();
+        int[][] array = new int[][]{{1, 2}, {4, 5}, {7, 8}, {9, 10}};
+        printArray(array);
     }
 }
